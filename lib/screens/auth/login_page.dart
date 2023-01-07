5094bc15c2ab9504aab3a83a233afd26efb6c1f7
Umama Tariq <102823252+umamaTariq@users.noreply.github.com>
2023-01-08 00:14:15 +0500

    add space

--- lib/screens/auth/login_page.dart
+++ lib/screens/auth/login_page.dart
@@ -113,6 +113,7 @@ class _LoginPageState extends State<LoginPage> {
                                 ],
                               ),
                             ),
+                            
                             Padding(
                               padding: const EdgeInsets.only(bottom: 4.0),
                               child: Text(
