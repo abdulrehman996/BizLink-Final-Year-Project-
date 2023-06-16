49a560895abcbada08f38f1ed28606a5341d3a1f
Abdul Rehman <58827592+abdulrehman996@users.noreply.github.com>
2023-06-16 19:07:04 +0500

    Revert "add auth files and other data"
    This reverts commit 14b4c25f26fac3af5d2d08c96a4046abdc3d7687.

--- lib/functions/time_function.dart
+++ lib/functions/time_function.dart
@@ -1,8 +1,6 @@
 import 'package:intl/intl.dart';
 
-class TimeDateFunctions {
-  static int get timestamp => DateTime.now().microsecondsSinceEpoch;
-
+class TimeFunction {
   static String timeInDigits(int timestamp) {
     DateFormat format = DateFormat('HH:mm a');
     DateTime date = DateTime.fromMicrosecondsSinceEpoch(timestamp);
